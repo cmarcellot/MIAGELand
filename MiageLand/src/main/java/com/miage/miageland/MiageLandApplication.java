804e7c2eb63d214e71644bf79413134729ec3a93
Camille Marcellot <camillemarcellot@gmail.com>
2023-05-24 16:59:06 +0200

    project init

---
+++ MiageLand/src/main/java/com/miage/miageland/MiageLandApplication.java
@@ -0,0 +1,14 @@
+package com.miage.miageland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiageLandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiageLandApplication.class, args);
+        System.out.println("MiageLandApplication started");
+    }
+
+}
---
+++ MiageLand/src/test/java/com/miage/miageland/MiageLandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miage.miageland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiageLandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
